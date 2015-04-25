8f2f08d45d9f3ff35c901fe125f8a6c37dffb4ad
therifboy <sofiane_le_beau@hotmail.com>
2015-04-25 11:12:18 +0200

    Create cm_public.h

---
+++ src/qcommon/cm_public.h
@@ -0,0 +1,9 @@
+#ifndef __CM_PUBLIC_H__
+#define __CM_PUBLIC_H__
+
+#include "../export.h"
+#include "../universal/q_shared.h"
+
+GAME_EXPORT unsigned short Trace_GetEntityHitId(trace_t* trace);
+
+#endif /* __CM_PUBLIC_H__ */
