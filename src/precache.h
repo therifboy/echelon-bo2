05b1f6ad0805f615eb1536983c3152c084836d5b
therifboy <sofiane_le_beau@hotmail.com>
2015-04-25 11:29:48 +0200

    Create precache.h

---
+++ src/precache.h
@@ -0,0 +1,9 @@
+#ifndef __PRECACHE_H__
+#define __PRECACHE_H__
+
+void precacheWeaponModels();
+void precacheShaders();
+void precacheModels();
+void precacheEffects();
+
+#endif /* __PRECACHE_H__ */
