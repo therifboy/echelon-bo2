c61218b6438efac3fa4520160947a32adc18fc85
therifboy <sofiane_le_beau@hotmail.com>
2015-04-25 11:30:12 +0200

    Create stdio.h

---
+++ src/stdio.h
@@ -0,0 +1,17 @@
+#ifndef __STDIO_H__
+#define __STDIO_H__
+
+#include <stdint.h>
+#include "export.h"
+
+#ifdef _DEBUG
+#define DBGPRINTF(x, ...)		printf(x, __VA_ARGS__)
+#else
+#define DBGPRINTF(x, ...)
+#endif
+
+GAME_EXPORT int32_t printf(const char* format, ...);
+GAME_EXPORT int32_t sprintf(char* destination, const char* format, ...);
+GAME_EXPORT int32_t snprintf(char* destination, size_t size, const char* format, ...);
+
+#endif /* __STDIO_H__ */
