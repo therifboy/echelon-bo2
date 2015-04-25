7ba3891924e6a4d17983ef9b2e1238e03fc3e471
therifboy <sofiane_le_beau@hotmail.com>
2015-04-25 11:04:36 +0200

    Create bg_local.h

---
+++ src/bgame/bg_local.h
@@ -0,0 +1,10 @@
+#ifndef __BG_LOCAL_H__
+#define __BG_LOCAL_H__
+
+// bg_local.h -- local definitions for the bg (both games) files
+
+#include "../universal/q_shared.h"
+
+extern dvar_s ** jump_slowdownEnable;
+
+#endif /* __BG_LOCAL_H__ */
